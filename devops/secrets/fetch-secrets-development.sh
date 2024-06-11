4f348568f4d1243542ebd617f551a8a76eefe791
sajjad-hanif82 <sajjad.hanif@maservices-ltd.com>
2024-06-11 14:44:11 +0500

    Google Secrets

---
+++ devops/secrets/fetch-secrets-development.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DB_HOST=$(gcloud secrets versions access latest --secret="dev-db-host")
+DB_PORT=$(gcloud secrets versions access latest --secret="dev-db-port")
+DB_DATABASE=$(gcloud secrets versions access latest --secret="dev-db-database")
+DB_USERNAME=$(gcloud secrets versions access latest --secret="dev-db-username")
+DB_PASSWORD=$(gcloud secrets versions access latest --secret="dev-db-password")
+
+echo "NODE_ENV=development" >> .env
+echo "APP_NAME=AIC API" >> .env
+echo "PORT=3500" >> .env
+
+echo "DB_HOST=$DB_HOST" >> .env
+echo "DB_PORT=$DB_PORT" >> .env
+echo "DB_DATABASE=$DB_DATABASE" >> .env
+echo "DB_USERNAME=$DB_USERNAME" >> .env
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
