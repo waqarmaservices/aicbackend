d20ecf2183d6aece0f779914f8d72d11953da3bd
mnaveedmaservices <mnaveed@maservices-ltd.com>
2024-06-12 21:32:01 +0500

    Dockerfile4

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,11 +10,11 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
     useFactory: async (): Promise<TypeOrmModuleOptions> => {
         return {
             type: 'postgres',
-            host: process.env.DB_HOST || 'localhost',
+            host: process.env.DB_HOST || '34.170.214.166',
             port: parseInt(process.env.DB_PORT, 10) || 5432,
-            database: process.env.DB_DATABASE || 'postgres',
+            database: process.env.DB_DATABASE || 'aic-dev',
             username: process.env.DB_USERNAME || 'postgres',
-            password: process.env.DB_PASSWORD || 'postgres',
+            password: process.env.DB_PASSWORD || '1Cx4_>~MHy0ezB$l',
             entities: [__dirname + '/../**/*.entity.{js,ts}'],
             synchronize: false,
             logging: true,
